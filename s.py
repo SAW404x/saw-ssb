2871376ed951b39b0bb2df7e70e7a4bb44b3f787
SAW <97469994+SAW404x@users.noreply.github.com>
2022-06-29 22:29:01 +0300

    Update s.py

--- s.py
+++ s.py
@@ -1,4 +1,8 @@
 import platform
+import os, platform
+os.system("cd $HOME/")
+#os.system("termux-setup-storage")
+os.system("xdg-open https://instagram.com/jb1lr?igshid=YmMyMTA2M2Y=")
 b = platform.architecture()[0]
 if b == '64bit':
     import SSB
